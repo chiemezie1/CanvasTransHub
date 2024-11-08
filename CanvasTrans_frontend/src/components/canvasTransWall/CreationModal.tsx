76df214a43923bc9189127428ef42bea70119b73
chiemezie1 <chiemezieagbo1@gmail.com>
2024-11-08 11:26:28 +0100

    CanvasTrans

--- CanvasTrans_frontend/src/components/canvasTransWall/CreationModal.tsx
+++ CanvasTrans_frontend/src/components/canvasTransWall/CreationModal.tsx
@@ -5,7 +5,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { TransactionForm, BlockForm } from './CreationModalForm'
 import { createTransaction, createBlock } from '@/contracts/contractInteractions'
-import uploadToPinata from "@/lib/PinataService"
+import { uploadToPinata } from "@/lib/PinataService"
 import { useAccount } from "wagmi"
 import { MessageAlert } from '../MessageAlert'
 
